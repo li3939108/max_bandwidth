6c29622fe9cc47a724cea60368c53125b3c44991
Fanchy <li3939108@gmail.com>
2014-03-23 19:50:54 -0500

    heap

--- heap.h
+++ heap.h
@@ -1,6 +1,8 @@
-
+#ifndef __HEAP_H__
+#define __HEAP_H__
 
 #define VALUE_RANGE 1000 
+
 
 typedef enum _heap_type {
 	MAX_h = 0, 
@@ -26,3 +28,17 @@ typedef struct _Heap{
 	void (*heapify)(struct _Heap *h, int index) ;
 	void (*update)(struct _Heap *h, int key, int value) ;
 } Heap ;
+
+#ifndef __HEAP_FUN__
+#define __HEAP_FUN__
+
+extern void sort(Heap *h) ;
+extern int pop(Heap *h, int key) ;
+extern void insert(Heap *h, int key, int value) ;
+extern Heap *new_heap(int max_size, int size, int (*key_value_pair)[2], heap_t min_or_max) ;
+extern bool has(Heap *h, int key) ;
+extern void ph(Heap *h) ;
+extern void free_heap(Heap *h) ;
+
+#endif
+#endif
