bbf69d0c88142bed153b2381f9456abdd7e2f271
Chaofan Li <li3939108@gmail.com>
2017-05-27 22:07:49 -0700

    small change

--- main.c
+++ main.c
@@ -223,7 +223,7 @@ int main(int argc, char *argv[])
     seed_vertices[0] = SEED;
     seed_vertices[1] = SEED + 1002;
     //multithread_infect(out2str);
-    stable_infect(INT_MAX, 0, MEAN);
+    stable_infect(INT_MAX, (unsigned int) (G->V * 3 / 5), MEAN);
 
     fclose(out_graph);
 
